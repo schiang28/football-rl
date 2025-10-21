b1bdbdfebcc2e8863da15c6b8fbd78b2c18ac1e5
Sophie Chiang <sophieyoshi2014@gmail.com>
2025-10-21 20:14:14 +0100

    first commit

---
+++ tutorial/play_scenario.py
@@ -0,0 +1,9 @@
+from vmas import make_env, render_interactively
+
+# Use the interactive render for debugging (runs a single env)
+print("Starting Interactive Render (Press R to reset, keys to control)")
+render_interactively(
+    scenario='football',
+    n_blue_agents=2, # can change number of controlled agents
+    n_red_agets=2 # passed but not used by the scenario
+)
